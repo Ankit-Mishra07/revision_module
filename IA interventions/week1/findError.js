136e149049799b008390f469b304d8c768407a26
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-08-08 14:52:00 +0530

    solve3

---
+++ IA interventions/week1/findError.js
@@ -0,0 +1,14 @@
+
+function checkPalindrome(str){
+		let left = 0;
+		let right = str.length - 1
+		while(left < right){
+				if (str[left] != str[right]){
+						return false;
+				}
+				// left++;
+				// right--
+		}
+		return true
+}
+console.log(checkPalindrome("naman"))
