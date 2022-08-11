fd6110bf7abbecdd250adbb2ddc95ce43a31c878
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-08-11 14:25:43 +0530

    server created

--- M5R3/index.js
+++ M5R3/index.js
@@ -1,25 +1,30 @@
-const url = "http://localhost:5000/json/data";
-function handleSubmitData() {
+const url = "http://localhost:3000/api/data";
+async function handleSubmitData(event) {
   event.preventDefault();
-  let country = document.getElementById("country");
-  let city = document.getElementById("city");
-  let population = document.getElementById("population");
+  let country = document.getElementById("country").value;
+  let city = document.getElementById("city").value;
+  let population = document.getElementById("population").value;
   let payload = {
     country,
     city,
     population,
   };
-  fetch(url, {
+  if (!country || !city || !population) {
+    alert("Please fill all data");
+    return;
+  }
+  console.log(payload);
+  await fetch("http://localhost:3000/api/data", {
     method: "POST",
     body: JSON.stringify(payload),
     headers: {
       "Content-Type": "application/json",
     },
-  })
-    .then((res) => res.json())
-    .then((res) => {
-      fetchData();
-    });
+  });
+  // .then((res) => res.json())
+  // .then((res) => {
+  //   fetchData();
+  // });
 }
 
 function fetchData() {}
