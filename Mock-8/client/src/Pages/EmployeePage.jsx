07e92ab52f0c4d69031e34648997db8f1757a692
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-08-18 13:54:00 +0530

    Employees listing page

--- Mock-8/client/src/Pages/EmployeePage.jsx
+++ Mock-8/client/src/Pages/EmployeePage.jsx
@@ -1,7 +1,60 @@
+import { Box } from "@chakra-ui/layout";
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import EmployeeCard from "../components/EmployeeCard";
+import { toast } from "react-toastify";
 
 const EmployeePage = () => {
-  return <div>EmployeePage</div>;
+  const [data, setData] = useState([]);
+  const fetchEmployees = async () => {
+    let res = await fetch("https://m5r3server.herokuapp.com/api/employee");
+    let data = await res.json();
+    setData(data);
+  };
+  useEffect(() => {
+    fetchEmployees();
+  }, []);
+
+  const handleDelete = (id) => {
+    fetch(`https://m5r3server.herokuapp.com/api/employee/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        fetchEmployees();
+        toast("Deleted Successfully");
+      })
+      .catch((error) => {
+        toast("Something went wrong");
+      });
+  };
+  return (
+    <Box
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "10px",
+        width: "60%",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "auto",
+        marginTop: "30px",
+      }}
+    >
+      {data?.map((property) => (
+        <Box
+          borderWidth="1px"
+          borderRadius="lg"
+          display="flex"
+          style={{ width: "100%" }}
+          key={property.id}
+        >
+          <EmployeeCard property={property} handleDelete={handleDelete} />
+        </Box>
+      ))}
+    </Box>
+  );
 };
 
 export default EmployeePage;
---
+++ Mock-8/client/src/components/EmployeeCard.jsx
@@ -0,0 +1,74 @@
+import { Button } from "@chakra-ui/button";
+import { Image } from "@chakra-ui/image";
+import { Badge, Box, Stack } from "@chakra-ui/layout";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const EmployeeCard = ({ property, handleDelete }) => {
+  return (
+    <>
+      <Box borderRadius="lg" display="flex" width={"100%"}>
+        <Image src={property.image} alt="uch" width={"50%"} />
+        <Box p="6">
+          <Box display="flex" alignItems="baseline">
+            <Badge borderRadius="full" px="2" colorScheme="teal">
+              {property.gender}
+            </Badge>
+            <Box
+              color="gray.500"
+              fontWeight="semibold"
+              letterSpacing="wide"
+              fontSize="xs"
+              textTransform="uppercase"
+              ml="2"
+            >
+              age &bull; {property.age} years
+            </Box>
+          </Box>
+
+          <Box
+            mt="1"
+            fontWeight="semibold"
+            as="h4"
+            lineHeight="tight"
+            noOfLines={1}
+          >
+            Name: {property.name}
+          </Box>
+
+          <Box>
+            Salary: {property.salary}
+            <Box as="span" color="gray.600" fontSize="sm">
+              / month
+            </Box>
+          </Box>
+          <Box
+            mt="1"
+            fontWeight="semibold"
+            as="h4"
+            lineHeight="tight"
+            noOfLines={1}
+          >
+            Department: {property.department}
+          </Box>
+          <Stack direction="row" spacing={4}>
+            <Button bg={"linkedin.400"} color="whiteAlpha.900">
+              {" "}
+              <Link to={`/employee/${property.id}`}>View</Link>
+            </Button>
+            <Button
+              bg={"red.400"}
+              color="whiteAlpha.900"
+              onClick={() => handleDelete(property.id)}
+            >
+              {" "}
+              Delete
+            </Button>
+          </Stack>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default EmployeeCard;
